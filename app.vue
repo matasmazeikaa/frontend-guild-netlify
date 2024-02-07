0c007f177d87800d79a604173275ef7b1691e48b
matasmazeikaa <matasmazeika@gmail.com>
2024-02-07 12:50:54 +0700

    add blog dispaly

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    Hi I'm a cool developer
+    <ul>
+      <li v-for="post in posts" :key="post.title">
+        <p>{{ post.title }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts" setup>
+
+// fetch blogs with nuxt/content plugin
+const { data: posts } = await useAsyncData('blog', () => queryContent('/blog').find())
+
+
+</script>
